7397c9050cd2a614ce36fdba15aa403a465bc015
Paul O Mahony <pomahoney@gmail.com>
2011-11-08 12:06:42 +0000

    removed redundant migration

--- db/migrate/20111030230323_add_student_id_to_notes.rb
+++
@@ -1,9 +0,0 @@
-class AddStudentIdToNotes < ActiveRecord::Migration
-  def self.up
-    add_column :notes, :student_Id, :integer
-  end
-
-  def self.down
-    remove_column :notes, :student_Id
-  end
-end
