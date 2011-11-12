56d99f5250e6ed9267053b62b0d9723b69ed2a81
Paul O Mahony <pomahoney@gmail.com>
2011-11-12 00:44:53 +0000

    added email to student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,12 @@ class Student < ActiveRecord::Base
 
   has_many :notes, :dependent => :destroy
   
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, :presence => true
+  validates :email, :presence => true, :format => {:with => email_regex}
+  
+  
 
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111107005335) do
+ActiveRecord::Schema.define(:version => 20111112002410) do
 
   create_table "notes", :force => true do |t|
     t.string   "note"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(:version => 20111107005335) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "email"
   end
 
 end
